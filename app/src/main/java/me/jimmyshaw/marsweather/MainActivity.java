b99c9a17950e21121c727ddc75c02994a6fa7e3a
Jim Shaw <jimxshaw@gmail.com>
2016-05-18 12:09:24 -0500

    added loadWeatherData and textError methods to MainActivity

--- app/src/main/java/me/jimmyshaw/marsweather/MainActivity.java
+++ app/src/main/java/me/jimmyshaw/marsweather/MainActivity.java
@@ -2,7 +2,15 @@ package me.jimmyshaw.marsweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -27,5 +35,60 @@ public class MainActivity extends AppCompatActivity
         mTextError = (TextView) findViewById(R.id.error);
     }
 
-    
+    private void loadWeatherData()
+    {
+        CustomJsonRequest request = new CustomJsonRequest(Request.Method.GET,
+                RECENT_API_ENDPOINT,
+                null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        try
+                        {
+                            String minTemp;
+                            String maxTemp;
+                            String atmo;
+                            int avgTemp;
+
+                            response = response.getJSONObject("report");
+
+                            minTemp = response.getString("min_temp");
+                            minTemp = minTemp.substring(0, minTemp.indexOf("."));
+
+                            maxTemp = response.getString("max_temp");
+                            maxTemp = maxTemp.substring(0, maxTemp.indexOf("."));
+
+                            avgTemp = (Integer.parseInt(minTemp) + Integer.parseInt(maxTemp)) / 2;
+
+                            atmo = response.getString("atmo_opacity");
+
+                            mTextDegrees.setText(avgTemp + "°");
+                            mTextWeather.setText(atmo);
+                        }
+                        catch (Exception e)
+                        {
+                            textError(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        textError(error);
+                    }
+                });
+
+        request.setPriority(Request.Priority.HIGH);
+        helper.add(request);
+    }
+
+    private void textError(Exception e)
+    {
+        mTextError.setVisibility(View.VISIBLE);
+        e.printStackTrace();
+    }
 }
